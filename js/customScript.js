ad846d37977eb64e77e05672e1cfb5619fd6a682
Jiu <imjiujiujiu@gmail.com>
2021-09-21 20:56:55 +0800

    RWD調整

--- js/customScript.js
+++ js/customScript.js
@@ -2,3 +2,11 @@
 $('#mainNav > ul > li').hover(function(){
 	$(this).toggleClass('hover')
 })
+
+$('#menu-bar').click(function(){
+	$('#phoneNav').addClass('open')
+})
+
+$('#close').click(function(){
+	$('#phoneNav').removeClass('open')
+})
